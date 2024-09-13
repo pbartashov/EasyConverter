4b1746eed2c3a6b53a5978d03cf11dc411e5cb41
pbartashov <p.bartashov@icloud.com>
2024-09-13 17:34:14 +0930

    Initial Commit

---
+++ EasyConverter/EasyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyConverterApp.swift
+//  EasyConverter
+//
+//  Created by Pavel Bartashov on 13/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
